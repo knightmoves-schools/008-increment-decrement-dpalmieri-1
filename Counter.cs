f23fe2406905b148d6def844b7af1dbb8a20c88b
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-01-30 11:36:51 -0600

    Update Counter.cs
    Should work.

--- Counter.cs
+++ Counter.cs
@@ -4,5 +4,9 @@ public class Counter
     public static int Visitors = 100;
 
     // Add your code here
-    
+    int PrefixIncrement = ++Visitors;
+    int PostfixIncrement = Visitors++;
+    int PrefixDecrement = --Visitors;
+    int PostfixDecrement = Visitors--;
 }
+
